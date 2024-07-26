c33b5c839ebe10decce22b5817e5b3b2b4087657
Yasir Rehman <yasir.rehman@curemd.com>
2024-07-26 17:48:00 +0500

    first commit

---
+++ src/main/java/autoconfigure/SecurityAutoConfiguration.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2021 Ona Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package autoconfigure;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * {@link EnableAutoConfiguration Auto-configuration} for HAPI FHIR.
+ *
+ * @author Reham Muzzamil
+ */
+@Configuration
+@AutoConfigureAfter({ KeycloakSecurityConfig.class })
+@Import({ KeycloakSecurityConfig.class, DisableSecurityConfig.class })
+public class SecurityAutoConfiguration {
+}
